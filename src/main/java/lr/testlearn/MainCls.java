39256ebcf839844c9126a3f05acbde234f24eb8d
Asif <asif_hossain@live.com>
2023-11-09 21:31:57 +0600

    changes

--- src/main/java/lr/testlearn/MainCls.java
+++ src/main/java/lr/testlearn/MainCls.java
@@ -1,10 +1,22 @@
 package lr.testlearn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MainCls {
+
+    private static final Logger log = LoggerFactory.getLogger(MainCls.class);
+
+    @Override
+    public String toString() {
+        return "MainCls{" + '}';
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        String toString = new MainCls().toString();
+
+        System.out.println(toString);
 
     }
 }
